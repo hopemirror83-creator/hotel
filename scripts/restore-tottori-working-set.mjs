45e046b9422fe642f8008b2310d32605b568bbac
스토리앤 <storyuman@gmail.com>
2026-08-19 20:26:02 +0900

    Add Tottori hotel expansion

---
+++ scripts/restore-tottori-working-set.mjs
@@ -0,0 +1,11 @@
+import { readFile, writeFile } from 'node:fs/promises';
+
+const hotels = JSON.parse(await readFile('data/target-hotels-tottori-v1-generated.json', 'utf8'));
+await writeFile('src/data/generatedHotels.ts', `// @ts-nocheck\nexport const generatedHotels: any[] = ${JSON.stringify(hotels, null, 2)};\n`);
+console.log(`Restored ${hotels.length} Tottori hotels to the working module`);
+
+
+
+
+
+
--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -49,6 +49,7 @@ import { iwateAreaGuides } from '../data/iwateAreaGuides';
 import { aomoriAreaGuides } from '../data/aomoriAreaGuides';
 import { hokkaidoAreaGuides } from '../data/hokkaidoAreaGuides';
 import { okayamaAreaGuides } from '../data/okayamaAreaGuides';
+import { tottoriAreaGuides } from '../data/tottoriAreaGuides';
 import { multilingualHotels } from '../data/multilingualHotels';
 import { multilingualRegions } from '../data/multilingualRegions';
 
@@ -107,6 +108,7 @@ export const GET: APIRoute = ({ site }) => {
     ...aomoriAreaGuides.map((guide) => guide.path),
     ...hokkaidoAreaGuides.map((guide) => guide.path),
     ...okayamaAreaGuides.map((guide) => guide.path),
+    ...tottoriAreaGuides.map((guide) => guide.path),
     ...gyeongbukAreaGuides.map((guide) => guide.path),
     ...gyeongnamAreaGuides.map((guide) => guide.path),
     ...hotels.map((hotel) => `/hotel/${hotel.slug}/`),

d0ee576741936a55e6e601e701519e9840951aeb
스토리앤 <storyuman@gmail.com>
2026-09-03 20:39:59 +0900

    Add Phu Tho and Viet Tri hotel expansion

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -91,6 +91,7 @@ import { bacninhAreaGuides } from '../data/bacninhAreaGuides';
 import { haiduongAreaGuides } from '../data/haiduongAreaGuides';
 import { bacgiangAreaGuides } from '../data/bacgiangAreaGuides';
 import { vinhphucAreaGuides } from '../data/vinhphucAreaGuides';
+import { phuthoAreaGuides } from '../data/phuthoAreaGuides';
 import { vungtauAreaGuides } from '../data/vungtauAreaGuides';
 import { hueAreaGuides } from '../data/hueAreaGuides';
 import { hoianAreaGuides } from '../data/hoianAreaGuides';
@@ -195,6 +196,7 @@ export const GET: APIRoute = ({ site }) => {
     ...haiduongAreaGuides.map((guide) => guide.path),
     ...bacgiangAreaGuides.map((guide) => guide.path),
     ...vinhphucAreaGuides.map((guide) => guide.path),
+    ...phuthoAreaGuides.map((guide) => guide.path),
     ...vungtauAreaGuides.map((guide) => guide.path),
     ...hueAreaGuides.map((guide) => guide.path),
     ...hoianAreaGuides.map((guide) => guide.path),
